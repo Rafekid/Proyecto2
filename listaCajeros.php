6a36dd3d566ca5c8a1bcd56436629b1346bdc092
davinciikart <davinciikart@gmail.com>
2020-09-22 19:19:49 -0600

    Se agrego listaCajeros.php

--- listaCajeros.php
+++ listaCajeros.php
@@ -16,6 +16,42 @@
 <html lang="en">
 <?php include "layouts/headAdmin.php";?>
 <body>
+    <?php 
+        include 'conexiones/conexion.php';
+        $sql="SELECT id_cajero,nom_comp,usuario,bloqueo FROM cajeros";
+        $resultado=mysqli_query($conexion,$sql) or die(mysql_error());
+    ?>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>NOMBRE</th>
+                <th>USUARIO</th>
+                <th>BLOQUEO</th>
+                <th>ACCIONES</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php 
+            while($filas = mysqli_fetch_assoc($resultado)) {
 
+            ?>
+            <tr>
+                <td><?php echo $filas['id_cajero']?></td>
+                <td><?php echo $filas['nom_comp']?></td>
+                <td><?php echo $filas['usuario']?></td>
+                <td><?php echo $filas['bloqueo']?></td>
+                <td>
+                <a href="id=<?php echo $filas['id_cajero']?>">Bloquear</a>
+                <a href="">Desbloquear</a>
+                </td>
+            </tr>
+            <?php 
+                    }
+            
+            $sql = null;
+            $conexion = null;?>
+        </tbody>
+    </table>
 </body>
 </html>
