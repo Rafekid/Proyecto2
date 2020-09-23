90c87295faf41191ae9afc75f1bb5b2cb546676e
davinciikart <davinciikart@gmail.com>
2020-09-22 18:17:44 -0600

    se agrego loginAdmin.php

--- loginAdmin.php
+++ loginAdmin.php
@@ -1,12 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <title>Administrador</title>
-</head>
+<?php include "layouts/headAdmin.php";?>
 <body>
     <form action="logica/verificarAdmin.php" method="POST">
         <h1>Log In de Administrador</h1>
