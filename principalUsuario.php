5103b5022d2ad54e50b2c15f5a8384c64db23c92
davinciikart <davinciikart@gmail.com>
2020-09-22 18:20:18 -0600

    Se agrego principalUsuario.php

--- principalUsuario.php
+++ principalUsuario.php
@@ -14,13 +14,7 @@
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <title>Usuario</title>
-</head>
+<?php include "layouts/headUsuario.php";?>
 <body>
     <div class="alert alert-success" role="alert">
     Bienvenido al portal de Usuario <?php echo $usuario?>
