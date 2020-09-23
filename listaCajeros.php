71cc55f7242c721f145a0b7e3dd3744e6bd87aa1
davinciikart <davinciikart@gmail.com>
2020-09-22 18:23:14 -0600

    Se creo listaCajeros.php

---
+++ listaCajeros.php
@@ -0,0 +1,21 @@
+<?php
+
+    session_start();
+
+    $usuario = $_SESSION['username'];
+
+    /*Aqui restringimos el acceso, si aún no se han logueado y matamos la session*/
+    if(!$usuario){
+        header("location: loginAdmin.php");
+        die();
+    } 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<?php include "layouts/headAdmin.php";?>
+<body>
+
+</body>
+</html>
