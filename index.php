98d414f20c2db230feb75a406c5919778e8f6c08
davinciikart <davinciikart@gmail.com>
2020-09-30 19:31:59 -0600

    Se agrega el enlace a loginAdmin.php

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
+<?php
+include "layouts/headUsuario.php"; ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
 <body>
-    <h1>Este es el Index del proyecto 2</h1>
+    <div class="contenedor">
+           <a href="loginAdmin.php">Enlace a login de Administrador</a>   
+    </div>
+ 
 </body>
+<?php include "layouts/footer.php" ?>
 </html>
